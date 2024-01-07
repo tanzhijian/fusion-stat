cad22789520566af6f436f248b4a4944eac47dc5
tanzhijian <i@tanzhijian.org>
2024-01-07 18:18:21 +0800

    refactor model.competitions

--- fusion_stat/models/competitions.py
+++ fusion_stat/models/competitions.py
@@ -1,4 +1,4 @@
-from rapidfuzz import process
+import typing
 
 from fusion_stat.config import COMPETITIONS
 
@@ -12,6 +12,16 @@ class PremierLeagueCompetitionDict(StatDict):
 class _BaseCompetitionParamsDict(ParamsDict):
     fbref_path_name: str | None
     official_name: str
+
+
+class InfoDict(typing.TypedDict):
+    count: int
+    names: list[str]
+
+
+class CompetitionDict(StatDict):
+    fotmob: StatDict
+    fbref: StatDict
 
 
 class CompetitionParamsDict(_BaseCompetitionParamsDict, total=False):
@@ -29,26 +39,78 @@ class Competitions:
         self.fbref = fbref
         self.season = season
 
-    def index(self) -> list[CompetitionParamsDict]:
+    @property
+    def info(self) -> InfoDict:
+        """
+        Return a dict that includes the following keys:
+
+        * count (int): number of competitions
+        * names (list[str]): names of competitions
+        """
+        return InfoDict(
+            count=len(COMPETITIONS),
+            names=list(COMPETITIONS.keys()),
+        )
+
+    @property
+    def items(self) -> list[CompetitionDict]:
+        """
+        Return a list of dicts that include the following keys:
+
+        * id (str): fotmob competition id
+        * name (str): config competition name
+        * fotmob (dict): fotmob competition
+                * id (str): fotmob competition id
+                * name (str): fotmob competition name
+        * fbref (dict): fbref competition
+                * id (str): fbref competition id
+                * name (str): fbref competition name
+        """
+        items: list[CompetitionDict] = []
+        for name, params in COMPETITIONS.items():
+            fotmob_competition = self._find_competition_by_id(
+                self.fotmob, params["fotmob_id"]
+            )
+            fbref_competition = self._find_competition_by_id(
+                self.fbref, params["fbref_id"]
+            )
+            item = CompetitionDict(
+                id=params["fotmob_id"],
+                name=name,
+                fotmob=fotmob_competition,
+                fbref=fbref_competition,
+            )
+            items.append(item)
+        return items
+
+    def _find_competition_by_id(
+        self, competitions: tuple[StatDict, ...], competition_id: str
+    ) -> StatDict:
+        for competition in competitions:
+            if competition["id"] == competition_id:
+                return competition
+        raise ValueError(f"Competition with id {competition_id} not found")
+
+    def get_params(self) -> list[CompetitionParamsDict]:
+        """
+        Return a list of dicts that include the following keys:
+
+            * fotmob_id (str): fotmob competition id
+            * fbref_id (str): fbref competition id
+            * fbref_path_name (str): fbref competition path name
+            * official_name (str): config competition name
+            * season (int, optional): fotmob competition season
+        """
         params: list[CompetitionParamsDict] = []
 
-        for fotmob_competition in self.fotmob:
-            fbref_competition = process.extractOne(
-                fotmob_competition,
-                self.fbref,
-                processor=lambda x: x["name"],
-            )[0]
-            official_name = process.extractOne(
-                fotmob_competition["name"],
-                COMPETITIONS.keys(),
-            )[0]
-
+        for item in self.items:
             competition_params = CompetitionParamsDict(
-                fotmob_id=fotmob_competition["id"],
-                fbref_id=fbref_competition["id"],
-                fbref_path_name=fbref_competition["name"].replace(" ", "-"),
-                official_name=official_name,
+                fotmob_id=item["fotmob"]["id"],
+                fbref_id=item["fbref"]["id"],
+                fbref_path_name=item["fbref"]["name"].replace(" ", "-"),
+                official_name=item["name"],
             )
+
             if self.season is not None:
                 competition_params["season"] = self.season
 
--- tests/models/test_competitions.py
+++ tests/models/test_competitions.py
@@ -2,6 +2,7 @@ import httpx
 import pytest
 
 from fusion_stat import Competitions
+from fusion_stat.models.base import StatDict
 from fusion_stat.spiders import fbref, fotmob
 from tests.utils import read_data
 
@@ -20,9 +21,31 @@ class TestCompetitions:
             fbref=fbref_spider.parse(httpx.Response(200, text=fbref_data)),
         )
 
-    def test_index(self, competitions: Competitions) -> None:
-        index = competitions.index()
-        competition = index[0]
+    def test_find_competition_by_id(self, competitions: Competitions) -> None:
+        stats_dict = (StatDict(id="1", name="a"),)
+        result = competitions._find_competition_by_id(stats_dict, "1")
+        assert result["name"] == "a"
+
+        with pytest.raises(ValueError):
+            competitions._find_competition_by_id(stats_dict, "a")
+
+    def test_items(self, competitions: Competitions) -> None:
+        items = competitions.items
+        assert len(items) == 5
+        competition = items[0]
+        assert competition["id"] == "47"
+        assert competition["fotmob"]["id"] == "47"
+        assert competition["fbref"]["id"] == "9"
+        assert (
+            competition["fotmob"]["name"]
+            == competition["fbref"]["name"]
+            == "Premier League"
+        )
+
+    def test_get_params(self, competitions: Competitions) -> None:
+        params = competitions.get_params()
+        assert len(params) == 5
+        competition = params[0]
         assert competition["fbref_path_name"] == "Premier-League"
         assert competition["official_name"] == "Premier League"
         with pytest.raises(KeyError):
