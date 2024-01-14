103df5d7295f8325f3e463a0c1b1ad94ecf5ea0f
tanzhijian <i@tanzhijian.org>
2024-01-14 23:18:00 +0800

    append tests: spider fotmob

--- tests/spiders/test_fotmob.py
+++ tests/spiders/test_fotmob.py
@@ -34,7 +34,11 @@ class TestCompetitions:
         data = read_test_data("allLeagues.json")
         response = httpx.Response(200, json=data)
         coms = spider.parse(response)
-        assert coms[0]["name"] == "Premier League"
+        assert len(coms) == 5
+
+        com = coms[0]
+        assert com["id"] == "47"
+        assert com["name"] == "Premier League"
 
 
 class TestCompetition:
@@ -44,33 +48,61 @@ class TestCompetition:
     ) -> typing.Generator[Competition, typing.Any, None]:
         yield Competition(id="47", client=client)
 
-    def test_request(
-        self, spider: Competition, client: httpx.AsyncClient
-    ) -> None:
+    def test_request(self, spider: Competition) -> None:
         assert spider.request.url == "https://www.fotmob.com/api/leagues?id=47"
 
-        spider_2022 = Competition(id="47", season=2022, client=client)
-        assert spider_2022.request
-
+    def test_request_include_season(
+        self, spider: Competition, client: httpx.AsyncClient
+    ) -> None:
+        spider = Competition(id="47", season=2022, client=client)
         assert (
-            spider_2022.request.url
+            spider.request.url
             == "https://www.fotmob.com/api/leagues?id=47&season=2022%2F2023"
         )
-        assert unquote(str(spider_2022.request.url)) == (
+        assert unquote(str(spider.request.url)) == (
             "https://www.fotmob.com/api/leagues?id=47&season=2022/2023"
         )
 
     def test_parse(self, spider: Competition) -> None:
-        # 待完善字段测试
         data = read_test_data("leagues?id=47.json")
         response = httpx.Response(200, json=data)
         com = spider.parse(response)
+        assert com["id"] == "47"
         assert com["name"] == "Premier League"
+        assert "Premier League" in com["names"]
+        assert com["type"] == "league"
+        assert com["season"] == "2023/2024"
         assert com["country_code"] == "ENG"
 
+        assert len(com["teams"]) == 20
+        team = com["teams"][0]
+        assert team["id"] == "8456"
+        assert team["name"] == "Manchester City"
+        assert "Man City" in team["names"]
+        assert team["played"] == 4
+        assert team["wins"] == 4
+        assert team["draws"] == 0
+        assert team["losses"] == 0
+        assert team["goals_for"] == 11
+        assert team["goals_against"] == 2
+        assert team["points"] == 12
+
+        assert len(com["matches"]) == 380
         match = com["matches"][0]
-        assert match["home"]["score"] == 0
-        assert match["away"]["score"] == 3
+        assert match["id"] == "4193450"
+        assert match["name"] == "Burnley vs Manchester City"
+        assert match["utc_time"] == "2023-08-11T19:00:00.000Z"
+        assert match["finished"]
+        assert match["started"]
+        assert not match["cancelled"]
+        home = match["home"]
+        assert home["id"] == "8191"
+        assert home["name"] == "Burnley"
+        assert home["score"] == 0
+        away = match["away"]
+        assert away["id"] == "8456"
+        assert away["name"] == "Manchester City"
+        assert away["score"] == 3
 
 
 class TestTeam:
@@ -88,14 +120,20 @@ class TestTeam:
         data = read_test_data("teams?id=9825.json")
         response = httpx.Response(200, json=data)
         team = spider.parse(response)
+        assert team["id"] == "9825"
         assert team["name"] == "Arsenal"
+        assert "Arsenal" in team["names"]
+
         assert len(team["members"]) == 26
         coach = team["members"][0]
         assert coach["is_staff"]
         player = team["members"][1]
         assert not player["is_staff"]
+        assert player["id"] == "562727"
+        assert player["name"] == "David Raya"
         assert player["position"] == "GK"
         assert player["country"] == "Spain"
+        assert player["country_code"] == "ESP"
 
 
 class TestMember:
@@ -113,7 +151,11 @@ class TestMember:
         data = read_test_data("playerData?id=961995.json")
         response = httpx.Response(200, json=data)
         member = spider.parse(response)
+        assert member["id"] == "961995"
         assert member["name"] == "Bukayo Saka"
+        assert member["country"] == "England"
+        assert member["position"] == "Right Winger"
+        assert not member["is_staff"]
 
 
 class TestMatches:
@@ -135,8 +177,18 @@ class TestMatches:
         match = matches[0]
         assert match["id"] == "4193495"
         assert match["name"] == "Crystal Palace vs Wolverhampton Wanderers"
-        assert match["home"]["score"] == 3
-        assert match["away"]["score"] == 2
+        assert match["utc_time"] == "2023-09-03T13:00:00.000Z"
+        assert match["finished"]
+        assert match["started"]
+        assert not match["cancelled"]
+        home = match["home"]
+        assert home["id"] == "9826"
+        assert home["name"] == "Crystal Palace"
+        assert home["score"] == 3
+        away = match["away"]
+        assert away["id"] == "8602"
+        assert away["name"] == "Wolverhampton Wanderers"
+        assert away["score"] == 2
 
 
 class TestMatch:
@@ -154,6 +206,7 @@ class TestMatch:
         data = read_test_data("matchDetails?matchId=4193490.json")
         response = httpx.Response(200, json=data)
         match = spider.parse(response)
+        assert match["id"] == "4193490"
         assert match["name"] == "Arsenal vs Manchester United"
 
 
